6a08dfc2244f6e4c0087dfd6a63a181c9a6af00d
christianmontealegre17 <christianmontealegre17@gmail.com>
2021-05-18 19:22:25 -0500

    local

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
-let numeros = [1, 500, 230, 40, 4, 65, 28, 32, 45, 1002]
-let un = [85, 65, 103, 25]
-orden1 = numeros.sort(function (a, b) { return a - b })
+function piedras() { 
+    let habas_cant = document.getElementById("piedras_cantidad").value
+    localStorage.setItem('piedras_cant_local', piedras_cant)
 
-console.log(orden1)
-orden2 = orden1.reverse()
-console.log(orden1)
+    let peso = document.getElementById("peso_user").value
+    sessionStorage.setItem("peso_piedra", peso)
 
-function elm1() {
-    console.log(orden1)
-    orden1.shift()
-    console.log(orden1)
 }
-
-function union() {
-    orden1.concat(orden2)
-    console.log(orden1)
-}
-
-    
-    orden1.push(85, 65, 103, 25)
-    console.log(orden1)
-
-
